ea64e8250825d481d54f1da6de2a27d08471154b
Kingsley Udochukwu <oparaudochukwu277@gmail.com>
2026-05-22 10:02:54 +0100

    fix a bug

--- backend/server.py
+++ backend/server.py
@@ -4,7 +4,7 @@ from flask import Flask
 from livekit.agents import WorkerOptions, cli
 from api import VoiceAssistant
 from livekit.plugins import google
-from livekit.agents import AgentSession, AutoSubscribe, JobContext, WorkerOptions, Worker
+from livekit.agents import WorkerOptions
 from dotenv import load_dotenv
 import sys
 
@@ -24,7 +24,7 @@ def run_agent():
     sys.argv = ["agent.py", "start"]
     from agent import enterypoint  # import your entrypoint function
     cli.run_app(WorkerOptions(entrypoint_fnc=enterypoint))
-    
+
 # Start the LiveKit agent worker in a background thread
 agent_thread = threading.Thread(target=run_agent, daemon=True)
 agent_thread.start()
